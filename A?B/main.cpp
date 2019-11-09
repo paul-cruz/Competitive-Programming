53a8ac12131ceb5ddc6ff2632794043ff7e30ccf
paul-cruz <43622955+paul-cruz@users.noreply.github.com>
2019-11-09 16:41:19 -0600

    Problem uploaded with C++ implementation

---
+++ A?B/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{	
+	int A,B;
+	cin>>A>>B;
+	cout<<A+B<<"\n";
+	cout<<A-B<<"\n";
+	cout<<A*B<<"\n";
+	cout<<A/B<<"\n";
+	cout<<A%B<<"\n";
+	return 0;
+}
